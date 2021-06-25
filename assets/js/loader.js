4eeac3821736452e296c234b81e7a57633ec1241
tezbot <86080874+tezbot@users.noreply.github.com>
2021-06-25 13:48:42 +0200

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,48 +1,40 @@
-$(document).ready(function() {
-  var counter = 0;
-
-  // Start the changing images
-  setInterval(function() {
-    if(counter == 9) { 
-      counter = 0; 
-    }
-
-    changeImage(counter);
-    counter++;
-  }, 3000);
-
-  // Set the percentage off
-  loading();
-});
-
-function changeImage(counter) {
-  var images = [
-    '<i class="fa fa-fighter-jet"></i>',
-    '<i class="fa fa-gamepad"></i>',
-    '<i class="fa fa-headphones"></i>',
-    '<i class="fa fa-cubes"></i>',
-    '<i class="fa fa-paw"></i>',
-    '<i class="fa fa-rocket"></i>',
-    '<i class="fa fa-ticket"></i>',
-    '<i class="fa fa-pie-chart"></i>',
-    '<i class="fa fa-codepen"></i>'
-  ];
-
-  $(".loader .image").html(""+ images[counter] +"");
-}
-
-function loading(){
-  var num = 0;
-
-  for(i=0; i<=100; i++) {
-    setTimeout(function() { 
-      $('.loader span').html(num+'%');
-
-      if(num == 100) {
-        loading();
-      }
-      num++;
-    },i*120);
-  };
-
-}
+$("html,body").animate({
+    scrollTop: 0
+}, "fast");
+$("html,body").scrollTop(0);
+$(window).on('load', async () => {
+    $("html,body").scrollTop(0);
+    await (new Promise((resolve, reject) => setTimeout(resolve, 1500)))
+    $("html,body").scrollTop(0);
+    $('.loader').remove()
+    $('.overflow').remove()
+    $('.circle-line').remove()
+    $('.circle-red').remove()
+    $('.circle-blue').remove()
+    $('.circle-green').remove()
+    $('.circle-yellow').remove()
+    document.querySelector('body').style.overflow = 'visible';
+    $("html,body").scrollTop(0);
+    $('.my').addClass('animate__animated')
+    $('.my').addClass('animate__bounceInLeft')
+    $('.color-text-flow').addClass('animate__animated')
+    $('.color-text-flow').addClass('animate__backInRight')
+    $('.desc').addClass('animate__animated')
+    $('.desc').addClass('animate__bounceInUp')
+    document.querySelectorAll('.yamete').forEach(i => {
+        $(i).addClass('animate__animated')
+        $(i).addClass('animate__bounceInUp')
+    })
+    setTimeout(() => {
+        $('.my').removeClass('animate__animated')
+        $('.my').removeClass('animate__bounceInLeft')
+        $('.color-text-flow').removeClass('animate__animated')
+        $('.color-text-flow').removeClass('animate__backInRight')
+        $('.desc').removeClass('animate__animated')
+        $('.desc').removeClass('animate__bounceInUp')
+        document.querySelectorAll('.yamete').forEach(i => {
+            $(i).removeClass('animate__animated')
+            $(i).removeClass('animate__bounceInUp')
+        })
+    }, 500)
+})
